336670d6dcc080dd628dee0bea286ab16ea76f55
Wesley Sousa Silva <wesley.sousa59@gmail.com>
2018-01-25 20:31:54 -0300

    Adicionado encripatão de senhas com o bcryptjs, no model e verificando se o usuário já existe antes de tentar inserir, no authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,10 +5,17 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+    const { email } = req.body;
     try {
+
+        if(await User.findOne({ email }))
+            return res.status(400).send({ error: 'User already exists' })
+
         const user = await User.create(req.body);
 
-        return res.send({ user });
+        user.password = undefined;
+
+        return res.status(201).send({ user });
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed' });
     }
